cd4de0314c75821bad89b12772d3b74dba1aa836
BryanReynoso1 <827627@mystma.org>
2021-02-10 12:12:37 -0600

    Car race
    Car Race

---
+++ Assets/Scenes/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    GameObject body;
+
+    public float camHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GameObject.Find("Head");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = new Vector3(body.transform.position.x,camHeight, -10);
+
+          
+    }
+}
---
+++ Assets/Scenes/Scripts/CarController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Rigidbody2D wheel_f, wheel_b;
+    public float power;
+    Vector3 startPos;
+    Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            wheel_f.AddTorque(-power);
+            wheel_b.AddTorque(-power);
+        }
+        if (Input.GetKey("a"))
+        {
+            wheel_f.AddTorque(power);
+            wheel_b.AddTorque(power);
+        }
+    }
+
+      private void OnCollisionEnter2D(Collision2D c)
+        {
+            if (c.gameObject.tag == "Respawn")
+            {
+                rb.velocity = Vector3.zero;
+                transform.position = startPos;
+            }
+        }
+    }
+
